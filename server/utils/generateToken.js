66cc339590263417c88d1a168dd5360a06f03ce6
khushi-1594 <kjindal414@gmail.com>
2025-04-02 22:16:10 +0530

    updated cookie config

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenandSetCookie = (userId, res)=>{
     res.cookie("jwt-cnnct",token, {
         maxAge: 20* 24*60*60*1000,
         httpOnly: true, //make sure that this cookie is only accessible via browser but not with js
-        sameSite: "strict",
+        sameSite: "None",
         secure: ENV_VARS.NODE_ENV !== "development"
     });
 
